277ba1af00d4eda2ba5f4ad7a745d560b7c8780b
rickbiopharma <109003197+rickbiopharma@users.noreply.github.com>
2023-09-10 19:58:23 -0400

    Revert "Update app.component.ts"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 <!-- * * * * * * * * * * test * * * * * * * * * * * * * * * * * -->
-  <!-- * * * * * * * * * * test2 * * * * * * * * * * * * * * * * * --> 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
