b9f2927237911c8b77caa8a8cccc515c4ae29125
rickbiopharma <109003197+rickbiopharma@users.noreply.github.com>
2023-09-10 19:26:56 -0400

    Update app.component.ts
    Second remark Test 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 <!-- * * * * * * * * * * test * * * * * * * * * * * * * * * * * -->
+  <!-- * * * * * * * * * * test2 * * * * * * * * * * * * * * * * * --> 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
